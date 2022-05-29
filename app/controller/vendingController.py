29ce9a88e5e44f53e7cc22cc2b1b43eb20c884b3
Wangdi988 <101056250+Wangdi988@users.noreply.github.com>
2022-05-29 12:52:37 -0400

    Add files via upload

---
+++ app/controller/vendingController.py
@@ -0,0 +1,63 @@
+from app import app
+from flask import jsonify, request
+from flask_cors import cross_origin
+from sqlalchemy import create_engine
+
+from flask_marshmallow import Marshmallow
+
+import pymysql
+
+
+@app.route("/available-item", methods=['GET'])
+@cross_origin()
+def itemList():
+    items = {
+        "Available": "Item list",
+        "items":[
+            {"Coke":"Nu: 25"},
+            {"chips":"Nu: 20"},
+            {"chocolate": "Nu: 5"}
+        ]
+    }
+    return jsonify(items)
+
+@app.route("/item", methods=['POST'])
+@cross_origin()
+def getItem():
+    json_request = request.json
+    item_name = json_request['item_name']
+    quantity = json_request['quantity']
+    amt = json_request['amount']
+
+    items = ["coke","chips", "chocolate"]
+    if (item_name in items):
+        if(item_name == "coke"):
+            cost = int(quantity) * 25
+            if(int(amt) == cost):
+                return jsonify({"Response":"Coke"})
+            elif(amt>cost):
+                blance = int(amt)-cost
+                return jsonify({"Your Choice":"Coke", "Your Blance": blance})
+            else:
+                return jsonify({"message":"Insufficent Amount", "Take Back your money":amt})
+        elif(item_name == "chips"):
+            cost = int(quantity) * 20
+            if(int(amt) == cost):
+                return jsonify({"Response":"Chips"})
+            elif(amt>cost):
+                blance = int(amt)-cost
+                return jsonify({"Your Choice":"Chips", "Your Blance": blance})
+            else:
+                return jsonify({"message":"Insufficent Amount", "Take Back your money":amt})
+
+        elif(item_name == "chocolate"):
+            cost = int(quantity) * 5
+            if(int(amt) == cost):
+                return jsonify({"Response":"Chocolate"})
+            elif(amt>cost):
+                blance = int(amt)-cost
+                return jsonify({"Response":"Chocolate", "Your Blance": blance})
+            else:
+                return jsonify({"message":"Insufficent Amount", "Take Back your money":amt})
+    else:
+        return jsonify({"This item is not available":item_name, "Take Back your money":amt})
---
+++ main.py
@@ -0,0 +1,5 @@
+from sqlalchemy import true
+from app import app
+
+if __name__ == ('__main__'):
+    app.run(debug=true)
